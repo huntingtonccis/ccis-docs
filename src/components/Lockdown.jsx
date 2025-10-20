35ed76ed33e702531a98f24b3a078247f950776c
jahardesty <jahardesty7@gmail.com>
2025-10-20 11:58:00 -0400

    css updates and Lockdown comp added

--- src/components/Lockdown.jsx
+++ src/components/Lockdown.jsx
@@ -8,25 +8,31 @@ const Lockdown = () => {
         navigate('/InstallDashboard');
     }
     return (
-        <body>
-        <h4>Lockdown Commissary Details</h4>
-        
-           <p>Run the installer</p><br/>
-            <p>For the shared path, put 172.30.30.192\lockdown</p><br />
+        <div className="grid-container">
+            <div className="header">
+        <h1>Lockdown</h1>
+        <button className='button' onClick={handleButtonClick}>Back to Install</button>
+        </div>
+        <div className='sidebar'>
+            <h4>Notes:</h4>
+            Lockdown is for Jail Commissary department.
+            This is physically stored
+            on a dell computer in the Jail server room
+            by IT office.  
+        </div>
+         <div className="main-content">
+            <h2>Install Instructions</h2>
+            <p>Run the installer - located here:</p>
+            <ul>
+            \\ccis22\install\Jail Install\LockdownInstall
+            </ul>
+            <p>For the shared path, put 172.30.30.192\lockdown</p>
             <ul>
             <li>ccis</li>
             <li>Huntington1!</li>
-            </ul> 
-        <button className='button' onClick={handleButtonClick}>Back to Install</button>
-        {/* <Button 
-             variant='secondary'
-             style={{
-              position: 'fixed',
-              top: '20px',
-              right: '20px',
-              zIndex: 1000
-             }} onClick={handleButtonClick}>Back to Install Directory</Button> */}
-   </body>
+            </ul>           
+         </div>
+        </div>        
     )
 }
 
